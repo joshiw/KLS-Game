93022e2fce02a3f56d398e3cbc44e8c6d1e5c9a1
TopGleo <l.gessner@gh-ai.de>
2024-05-21 11:11:31 +0200

    kartenfeld

--- karte.py
+++ karte.py
@@ -11,41 +11,25 @@ WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Spielerposition und Geschwindigkeit
-background_pos = [WIDTH // 2, HEIGHT // 2]
-player_speed = 5
-
-# Erstellen des Fensters
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-spielerfigur = pygame.image.load("static/images/walküre.jpg")
-pygame.display.set_caption("Raumschiff Bewegung")
-
-# Hauptspiel-Schleife
-running = True
-while running:
-    # Ereignisüberwachung
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Spielerbewegung
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        background_pos[0] += player_speed
-    if keys[pygame.K_RIGHT]:
-        background_pos[0] -= player_speed
-    if keys[pygame.K_UP]:
-        background_pos[1] += player_speed
-    if keys[pygame.K_DOWN]:
-        background_pos[1] -= player_speed
-
-    # Spielfeld zurücksetzen
-    screen.fill((0,0,0))
-
-    screen.blit(spielerfigur,background_pos)
-
-    # Spieler zeichnen
-    pygame.draw.circle(screen, WHITE, (400,300), 20)
+Karte=[
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0]
+]
 
     # Bildschirm aktualisieren
     pygame.display.flip()
