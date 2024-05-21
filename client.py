b5e14c623bf7852f9da03e45ec67b69f34adc59a
juliana2222 <jcsr7@web.de>
2024-05-21 11:31:41 +0200

    Stand 21.5.

--- client.py
+++ client.py
@@ -1,4 +1,26 @@
-import socket
+import socket, pygame
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+
+def player():
+    def _init_(self, x, y, width, hight, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.hight = hight
+        self.color = color
+        self.rect = (x, y, width, hight)
+
+def draw(self, win):
+    pygame.draw.rect(win, self.color, self.rect)
+
+def move(self):
+    
+
+def window():
+    win.fill((255,255,255))
 
 def main():
     host = '127.0.0.1'
@@ -6,3 +28,11 @@ def main():
 
     server_connection = socket.socket()
     server_connection.bind((host, port))
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
