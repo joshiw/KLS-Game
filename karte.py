9616c900cb2233bc301d4ce817943efc5544418c
Alessandro123456789012 <ihaveanewpc2@gmail.com>
2024-05-21 11:32:49 +0200

    mauer

--- karte.py
+++ karte.py
@@ -1,15 +1,26 @@
-import pygame
-import sys
+import pygame.locals
+import sys,time,random
 
 # Initialisierung von  Pygame
 pygame.init()
 
-# Fenstergröße
-WIDTH, HEIGHT = 800, 600
+# unser Multiplikator 
+MULTIPLIKATOR = 20
 
-# Farben
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+# Spielfeld erzeugen über Berechnung
+fenster = pygame.display.set_mode((20 * MULTIPLIKATOR, 30 * MULTIPLIKATOR))
+
+# Titel für Fensterkopf
+pygame.display.set_caption("Breakout in Python")
+spielaktiv = True
+
+# Bildschirm Aktualisierungen einstellen
+clock = pygame.time.Clock()
+
+# genutzte Farben
+ORANGE  = ( 255, 140,   0)
+SCHWARZ = (   0,   0,   0)
+WEISS   = ( 255, 255, 255)
 
 Karte=[
     [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
@@ -30,15 +41,34 @@ Karte=[
     [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0]
 ]
+# Korrekturfaktor berechnen
+def kor(zahl):
+    zahl = zahl * MULTIPLIKATOR
+    return zahl
 
-    # Bildschirm aktualisieren
+# Spielelement zeichnen
+def element_zeichnen(spalte,reihe):
+    pygame.draw.rect(fenster, ORANGE, [kor(spalte)+1, kor(reihe)+1,kor(1)-1,kor(1)-1])
+
+# Ausgabe Mauersteine im Spielfenster
+for x in range(0,16):
+    for y in range(0,16):
+        if Karte[y][x] == 0:
+            element_zeichnen(x,y)
+
+# Schleife Hauptprogramm
+while spielaktiv:
+    # Überprüfen, ob Nutzer eine Aktion durchgeführt hat
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            spielaktiv = False
+            print("Spieler hat beendet")
+    element_zeichnen(2,3)
+    # Fenster aktualisieren
     pygame.display.flip()
 
-    # Bildschirm aktualisieren
-    pygame.display.update()
-
-    # Framerate einstellen
-    pygame.time.Clock().tick(60)
+    # Refresh-Zeiten festlegen
+    clock.tick(10)
 
 # Pygame beenden
 pygame.quit()
